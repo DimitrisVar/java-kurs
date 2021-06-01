932002187139c27fdefa2ef62fa0ed6dfc0a1cc7
DimitrisVar <contact@dimitrisvar.com>
2021-06-01 15:38:04 +0200

    files added

--- Java-Kurs-inputOutput/src/Aufgabe_3.java
+++ Java-Kurs-inputOutput/src/Aufgabe_3.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class Aufgabe_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Aufgabe 
+		/*
+		 * Erstelle erneut ein Objekt der Klasse Scanner
+		 * und lese 2 Werte mithilfe dieser Klasse ein
+		 * 
+		 * 1. ist eine Ganzzahl, die Zahl soll dein Alter sein
+		 * 2. ist eine Zeichenkette(String), dein Namen
+		 * 
+		 * Am Ende soll dein Alter und dein Namen in der Konsoles ausgegeben werden
+		 * 
+		 */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Geben Sie Ihr Alter ein: ");
+		
+		int alter = scan.nextInt();
+		
+		System.out.println("Geben Sie Ihr Namen ein: ");
+		
+		String name = scan.next();
+		
+		System.out.println("Name: " +name +"\n" +"Alter: " +alter);
 	}
 
 }
--- Java-Kurs-inputOutput/src/Einlesen1.java
+++ Java-Kurs-inputOutput/src/Einlesen1.java
@@ -5,18 +5,20 @@ public class Einlesen1 {
 		
 		// Klasse = Koplexer Datentyp sprich dort sind mehr als nur eine Information gespeichert
 		
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Bitte eine Zahl eingeben: " );
-		
-		double zahl = scan.nextDouble();
-		System.out.println("Ihre Zahl lautet: " +zahl);
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Bitte eine Zahl eingeben: " );
+//		double zahl = scan.nextDouble();
+//		System.out.println("Ihre Zahl lautet: " +zahl);
 		
 		Scanner scan2 = new Scanner(System.in);
+		
 		System.out.println("Geben Sie Ihren Namen ein: ");
+		
 		String zeichenKette = scan2.next();
+		
 		System.out.println("Bitte geben Sie Ihr Alter ein: ");
 		int alter = scan2.nextInt();
 		
-		System.out.println("Ihr Name:" +zeichenKette + " Ihr Alter: " +alter);
+		System.out.println("Ihr Name: " +zeichenKette + " Ihr Alter: " +alter);
 	}
 }
