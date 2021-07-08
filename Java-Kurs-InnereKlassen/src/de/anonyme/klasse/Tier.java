c97246903d772f5ef4c4fc4b4c7bf58555135c55
DimitrisVar <contact@dimitrisvar.com>
2021-07-08 17:07:45 +0200

    interface commend added

--- Java-Kurs-InnereKlassen/src/de/anonyme/klasse/Tier.java
+++ Java-Kurs-InnereKlassen/src/de/anonyme/klasse/Tier.java
@@ -1,6 +1,8 @@
 package de.anonyme.klasse;
 
 public interface Tier {
+	
+	// Ein Interface mit nur EINER Methode Signatur / Kopf nennt man ein funktionales Interface.
 
 	public String machLaute();
 	
